d68257ec055bdf85f13e92b2587be3a5bd234d36
Julian Fiander <julian.fiander@mangolanguages.com>
2018-07-27 18:01:58 -0400

    Move FA::Layer logic into raw

--- lib/fa/layer.rb
+++ lib/fa/layer.rb
@@ -8,16 +8,21 @@ module FA
       #
       # Note: scaling counters does not work well with :grow, so should use the
       # older "fa-3x" syntax in :css instead.
-      span_top = "<span class='icon fa-layers fa-fw #{css}' title='#{title}'>"
-      span_bottom = '</span>'
-
-      icons.each { |i| i[:options] = combine_options(i, combine_grows(i, grow)) }
-
-      @output = span_top + parse_all(icons).join + span_bottom
+      @icons = icons
+      @title = title
+      @grow = grow
+      @css = css
     end
 
     def raw
-      @output
+      span_top = "<span class='icon fa-layers fa-fw #{@css}' title='#{@title}'>"
+      span_bottom = '</span>'
+
+      @icons.each do |i|
+        i[:options] = combine_options(i, combine_grows(i, @grow))
+      end
+
+      span_top + parse_all(@icons).join + span_bottom
     end
   end
 end
